f6ed3164f89be7114750360e0156ddcd41fe92f6
dimagong <dimagong@gmail.com>
2023-04-29 17:41:23 +0200

    refactor(selector): change option for date data

--- src/store/selectors/selectors.tsx
+++ src/store/selectors/selectors.tsx
@@ -7,11 +7,21 @@ export const selectNextMatch = createSelector(
     const teamsDetails = value.value[0] as any;
     const teamHome = teamsDetails?.teams[0].name ?? 'No team';
     const teamGuest = teamsDetails?.teams[1].name ?? 'No team';
-    const options: any = {year: '2-digit', month: 'numeric', day: 'numeric'};
+    // const options: any = {year: '2-digit', month: 'numeric', day: 'numeric'};
+    const optionsTime: any = {
+      year: '2-digit',
+      month: 'numeric',
+      day: 'numeric',
+      hour: '2-digit',
+      hour12: false,
+      minute: '2-digit',
+    };
 
-    const meetingTime =
-      new Date(teamsDetails?.startDate).toLocaleDateString('en-US', options) ??
-      new Date().toLocaleDateString('en-US', options);
-    return {teamHome, teamGuest, meetingTime};
+    const time =
+      new Date(teamsDetails?.startDate).toLocaleDateString(
+        'en-US',
+        optionsTime,
+      ) ?? new Date().toLocaleDateString('en-US', optionsTime);
+    return {teamHome, teamGuest, time};
   },
 );
