6ca2c7c4a6420b0d007939fff503fcec553ded17
dimagong <dimagong@gmail.com>
2023-05-03 14:45:50 +0200

    feat(interfaces): add new types for the data about football matchs

--- src/interfaces/interfaces.ts
+++ src/interfaces/interfaces.ts
@@ -1,3 +1,21 @@
 export interface Navigation {
-    navigate(destination: string, params?: any): void;
-  }
+  navigate(destination: string, params?: any): void;
+}
+
+export interface IMatch {
+  homeaway: 'home' | 'away';
+  match: {
+    startDate: string;
+    __issfId: number;
+    _id: string;
+  };
+  matchId: string;
+  result: 'L' | 'D' | 'W';
+  score: string;
+  team: {
+    displayName: string;
+    name: string;
+    resignation: boolean;
+    _id: string;
+  };
+}
