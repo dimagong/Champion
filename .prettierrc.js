446d578db4b8fc251e26605049d8ff7b3ff7882f
dimagong <dimagong@gmail.com>
2024-07-24 18:18:52 +0200

    common: change prettier setting

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,13 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
-  singleQuote: true,
-  trailingComma: 'all',
+    arrowParens: 'avoid',
+    bracketSameLine: true,
+    bracketSpacing: false,
+    singleQuote: true,
+    trailingComma: 'all',
+    tabWidth: 4,
 };
+
+// "trailingComma": "es5",
+//   "tabWidth": 4,
+//   "semi": false,
+//   "singleQuote": true
